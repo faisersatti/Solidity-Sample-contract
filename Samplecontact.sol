21375156bcac0cbf9a6b8b9401e0cfc017e8fc6b
Faiser Mehmood <faiser.satti@gmail.com>
2018-10-26 23:26:36 +0500

    Update Samplecontact.sol

--- Samplecontact.sol
+++ Samplecontact.sol
@@ -1,4 +1,18 @@
 pragma solidity ^0.4.22;
-contract SimpleContract{
-    
+contract SimpleContract
+{  //Data members in solidity 
+   string private Name;
+   uint256 private Age;
+ function setName(string name)public {
+     Name=name;
+ }
+ function setName(uint age)public {
+     Age=age;
+ }
+ function getName()public returns(string){
+ return Name;
+ }
+ function getAge()public returns(uint256){
+ return Age;
+ }
 }
